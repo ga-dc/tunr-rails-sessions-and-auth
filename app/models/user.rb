760d108fd36f33fc605e0073c1d7441491ea6db6
RobertAKARobin <robertgfthomas@gmail.com>
2015-07-28 15:08:50 -0400

    Tidied user model a bit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-  has_many :artists
   has_secure_password
+  has_many :artists, dependent: :destroy
 end
